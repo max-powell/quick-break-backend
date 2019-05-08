7ec02393728fca05e6b5570de6b26fb800c30ab5
max-powell <github email address>
2019-05-08 17:21:00 +0100

    trip serializer displays array of unavailable dates

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,3 +1,7 @@
 class TripSerializer < ActiveModel::Serializer
-  attributes :id, :name, :month, :duration, :users
+  attributes :id, :name, :month, :duration, :users, :unavailable_dates
+
+  def unavailable_dates
+    object.unavailable_dates.map(&:date)
+  end
 end
