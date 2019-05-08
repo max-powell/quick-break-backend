6cd41e25221f8ac9b15905a644195336b0af7bce
max-powell <github email address>
2019-05-08 17:58:54 +0100

    moved unavailability under trip

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  resources :user_trips, except: [:new, :edit] do
-    resources :unavailable_dates, only: [:index, :create, :destroy]
-  end
+  resources :user_trips, except: [:new, :edit]
   resources :trips, except: [:new, :edit] do
     resources :locations, only: [:index, :create, :update]
+    resources :unavailable_dates, only: [:index, :create, :destroy]
   end
   resources :users, only: [:index, :create, :destroy]
 
